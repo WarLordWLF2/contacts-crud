2fdea900d5303ba0861113bdc9c26b2bdb3f68ab
WarLordWLF2 <158793263+WarLordWLF2@users.noreply.github.com>
2024-09-09 10:11:35 +0800

    Delete contact directory

--- contact/headers.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Method: GET, POST, PUT, OPTIONS");
-header("Access-Control-Allow-Method: Content-Type, Authorization");
